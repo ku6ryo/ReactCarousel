a4c4986b5b60a4000b29b5717544be5506c0a317
Ryo Kuroyanagi <ku6ryo@gmail.com>
2023-01-22 14:50:21 +0900

    changed the section title

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <div className={style.frame}>
-      <h1>Basic</h1>
+      <h1>Pure base</h1>
       <CarouselBase
         slideTime={500}
         itemsPerPage={2}
